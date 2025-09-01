b28f57440714fa50ee1dd607c6cdd4c57ab2c343
Kenneth <36460677+CheeseManzs@users.noreply.github.com>
2025-09-01 16:19:49 -0400

    Changed to icon-based system

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-
+import {Grid, Item} from '@mui/material';
 class Showcase extends React.Component
 {
   constructor(props)
@@ -11,12 +11,18 @@ class Showcase extends React.Component
     this.name = props.internalName;
     this.url = props.link;
     this.banner = props.bannerURL;
+    this.baseWidth = props.baseWidth;
   }
 
   render() {
-    return <div style={{width:"100vw",overflow:"hidden",height:"20vh",alignContent:"center",display:"grid"}}>
-      <a href={this.url}><img className='banner' src={this.banner} alt={this.internalName}/></a>
+    return (
+      <div className='appIcon' style={{display:"grid", alignContent:"center"}}>
+      <div style={{width:"10vw",overflow:"hidden",height:"10vw",alignItems:"center",display:"grid",borderRadius:"1vw", backgroundColor:"#212121"}}>
+      <a href={this.url} style={{width:"100%", height:"100%", alignItems:"center", justifyContent:"center", display:"block", margin:"auto", scale:this.baseWidth}}><img className='banner' src={this.banner} alt={this.name} style={{width:"100%", alignSelf:"center"}}/></a>
       </div>
+      <h5>{this.name}</h5>
+      </div>
+      )
   }
 }
 
@@ -26,10 +32,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='floater'><img src="CheeseMansBanner+.png" className='titleBanner'/></div>
-        <div style={{top:"25vh", position:'absolute'}}>
-        <Showcase bannerURL="3subanner_reg.png" link="https://drive.usercontent.google.com/download?id=1QdXoN7_lgOYB18ryaEgkzA2sW8tA3-jE&export=download&authuser=0" internalName="3 Sides Ultra" />
-        <Showcase bannerURL="obr_banner.png" link="WebGL/orbiters/index.html" internalName="Orbiters" />
-        <Showcase bannerURL="cannon_duels_banner.png" link="https://drive.usercontent.google.com/download?id=18FanbB_shT1fUgI2uDPIvvcWvgnHHha8&export=download&authuser=0" internalName="Cannaon Duels" />
+        <div style={{display:"grid", alignContent:"center", columnCount:"5", gridTemplateColumns:"auto auto auto", columnGap:"10vw"}}>
+        <Showcase bannerURL="3sulogo.png" link="https://drive.usercontent.google.com/download?id=1QdXoN7_lgOYB18ryaEgkzA2sW8tA3-jE&export=download&authuser=0" internalName="3 Sides Ultra" baseWidth="100%" />
+        <Showcase bannerURL="cannon_duels_logo.png" link="https://drive.usercontent.google.com/download?id=18FanbB_shT1fUgI2uDPIvvcWvgnHHha8&export=download&authuser=0" internalName="Cannon Duels" baseWidth="100%"/>
+        <Showcase bannerURL="orbiters_templogo.png" link="WebGL/orbiters/index.html" internalName="ORBITERS" baseWidth="130%"/>
         </div>
       </header>
     </div>
